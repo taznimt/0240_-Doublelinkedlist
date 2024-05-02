d2322cd3edafa7f97ee4ef5e6f97acc63d3e75d7
taznimt <taznimanwar06@gmail.com>
2024-05-02 15:50:33 +0700

    Accept number of the student

--- doublelinklist/doublelinklist.cpp
+++ doublelinklist/doublelinklist.cpp
@@ -18,6 +18,7 @@ Node* START = NULL;
 void addNode() {
 	Node* newNode = new Node(); // step1 : create a new mode
 	cout << "\nEnter the roll number of the student: ";
+	cin >> newNode->noMhs; //Assign value to the data field of the new node
 }
 
 int main()

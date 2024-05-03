b30b6f668615159c67efb133b4c46dcea55f0462
taznimt <taznimanwar06@gmail.com>
2024-05-03 21:04:40 +0700

    create prosedure deleteNode

--- doublelinklist/doublelinklist.cpp
+++ doublelinklist/doublelinklist.cpp
@@ -82,6 +82,61 @@ bool search(int rollNo, Node** previous, Node** current)
 	return (*current != NULL);
 }
 
+void deleteNode()
+{
+	Node* previous, * current; //
+	int rollNo;
+
+	cout << "\nEnterthe roll number of the student whose record is to be deleted: ";
+	cin >> rollNo; // step 3: get the roll number to be deleted
+
+	if (START == NULL)
+	{
+		cout << "List is empty" << endl;
+		return;
+	}
+
+	current = START; // step 1: start from the first node
+	previous = NULL;
+
+	// Locate the node to be deleted
+	while (current != NULL && current->noMhs != rollNo)
+	{
+		previous = current;
+		current = current->next;
+	}
+
+	if (current == NULL)
+	{
+		cout << "\033[31mThe record with roll number " << rollNo << "not found\033[0m" << endl;
+		return;
+	}
+
+	// Node to be deleted is the first node
+	if (current == START)
+	{
+		START = START->next; // step 2: update the START pointer
+		if (START != NULL)
+		{
+			START->prev = NULL;
+		}
+	}
+	else
+	{ // Node to be deleted is not the first node
+		previous->next = current->next;
+		if (current->next != NULL)
+		{ // if there's a successor, update its prev pointer
+			current->next->prev = previous;
+
+		}
+
+	}
+
+	// Release the memory of the node marked as current
+	delete current;
+	cout << "\x1b[32mRecord with roll number " << rollNo << " deleted\x1b[0m" << endl;
+}
+
 int main()
 {
    

15a821f407b1eebb4504732134fc706553252142
taznimt <taznimanwar06@gmail.com>
2024-05-03 20:03:56 +0700

    memindahkan node saat ini ke node berikutnya

--- doublelinklist/doublelinklist.cpp
+++ doublelinklist/doublelinklist.cpp
@@ -47,6 +47,7 @@ void addNode() {
 		while (current != NULL && current->noMhs < newNode->noMhs)
 		{                        // step 1.c: traverse the list to find the correct position
 			previous = current; // step 1.d: move the previous to the current node
+			current = current->next; //step 1.e: move the current to the next node
 			
 		}
 	}

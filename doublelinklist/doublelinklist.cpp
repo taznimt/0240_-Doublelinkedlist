ab55b9d3ed5ad25b253052acf8c152048e6ede64
taznimt <taznimanwar06@gmail.com>
2024-05-03 19:20:08 +0700

    membuat node baru sebagai node pertama

--- doublelinklist/doublelinklist.cpp
+++ doublelinklist/doublelinklist.cpp
@@ -36,6 +36,7 @@ void addNode() {
 			START->prev = newNode; // step 4: make the first node point to the new node
 		}
 		newNode->prev = NULL; // step 5: make the new node point to NULL
+		START = newNode; // step 6: make the new node the first node
 	}
 }
 

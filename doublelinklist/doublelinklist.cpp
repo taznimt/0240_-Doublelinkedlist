ecbb975538bb0399ac5b8f7299c872beb2f35b2a
taznimt <taznimanwar06@gmail.com>
2024-05-03 19:18:27 +0700

    membuat node baru sama dengan nol

--- doublelinklist/doublelinklist.cpp
+++ doublelinklist/doublelinklist.cpp
@@ -35,7 +35,7 @@ void addNode() {
 		{
 			START->prev = newNode; // step 4: make the first node point to the new node
 		}
-
+		newNode->prev = NULL; // step 5: make the new node point to NULL
 	}
 }
 

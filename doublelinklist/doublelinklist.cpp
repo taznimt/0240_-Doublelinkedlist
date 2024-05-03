6d140e63f4c4c9cea10d57c92c7d80e97cc84126
taznimt <taznimanwar06@gmail.com>
2024-05-03 20:22:05 +0700

    jika node sebelumnya tetap null, berarti node baru = node pertama

--- doublelinklist/doublelinklist.cpp
+++ doublelinklist/doublelinklist.cpp
@@ -62,6 +62,12 @@ void addNode() {
 		{
 			previous->next = newNode; // step 7: make the next field of the previous node point to the new node
 		}
+		else
+		{
+			// if previous still null, it means newNode is now the first node
+			START = newNode;
+		}
+
 	}
 }
 

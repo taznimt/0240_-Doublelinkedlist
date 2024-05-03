86cbdb622ceae196346aef75dfcda5069ee756e5
taznimt <taznimanwar06@gmail.com>
2024-05-03 21:14:18 +0700

    create prosedure traverse

--- doublelinklist/doublelinklist.cpp
+++ doublelinklist/doublelinklist.cpp
@@ -140,6 +140,21 @@ bool listEmpty()
 {
 	return (START == NULL);
 }
+void traverse()
+{
+	if (listEmpty())
+		cout << "\nList is empty" << endl;
+	else
+	{
+		cout << "\nRecord in ascending order of roll number are: " << endl;
+		Node* currentNode = START; // Step 1
+		while (currentNode != NULL) // step 2
+		{
+			cout << currentNode->noMhs << " " << currentNode->name << endl; // step 3
+			currentNode = currentNode->next;                                // step 4
+		}
+	}
+}
 
 int main()
 {

e7e7e6833a04ca4d5ebfa9791a59099119987287
taznimt <taznimanwar06@gmail.com>
2024-05-03 20:29:24 +0700

    create fungsi search

--- doublelinklist/doublelinklist.cpp
+++ doublelinklist/doublelinklist.cpp
@@ -70,6 +70,17 @@ void addNode() {
 
 	}
 }
+bool search(int rollNo, Node** previous, Node** current)
+{
+	*previous = NULL;
+	*current = START;
+	while (*current != NULL && (*current)->noMhs != rollNo)
+	{
+		*previous = *current;
+		*current = (*current)->next;
+	}
+	return (*current != NULL);
+}
 
 int main()
 {
